7b4ba088e139807c8dfb57ee05fd006f61e43441
tquean15012003 <tquean15012003@gmail.com>
2022-09-27 10:42:08 +0800

    Initial Commit

---
+++ DLW/DLWApp.swift
@@ -0,0 +1,17 @@
+//
+//  DLWApp.swift
+//  DLW
+//
+//  Created by Que An Tran on 27/9/22.
+//
+
+import SwiftUI
+
+@main
+struct DLWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
